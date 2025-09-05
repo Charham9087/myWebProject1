a136e617867035e7857d7ba4e6e9df9b45e8795d
Charham9087 <charhamqaiser45678@gmail.com>
2025-09-05 22:59:34 +0500

    hello carousil ko remove kiya hai2

--- components/FrontendComponents/HomePageComponents/carousel/page.jsx
+++ components/FrontendComponents/HomePageComponents/carousel/page.jsx
@@ -1,11 +1,11 @@
 "use client";
-import { useState, useEffect } from "react";
+
 
 export default function Carousel() {
 
   return (
           <h1 className={`
-          text-4xl md:text-5xl font-extrabold text-center drop-shadow-lg animate-fadeSlide text-transparent bg-clip-text
+          text-4xl md:text-5xl font-extrabold text-center drop-shadow-lg animate-fadeSlide
         `}>
           Welcome to Ghari Point
         </h1>
