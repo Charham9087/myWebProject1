ecb7749dc59fc43080a8fa0413c0719c0ebf4037
Charham9087 <charhamqaiser45678@gmail.com>
2025-10-05 00:55:26 +0500

    meta pexels added

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import LayoutWrapper from "@/components/LayoutWrapper";
 import { EdgeStoreProvider } from "@/components/edgestore";
 import { Toaster } from 'react-hot-toast'
 import NEXT_AUTH_PROVIDER from "@/components/NextAuthProvider";
-
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -17,19 +17,48 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        
+      <head>
+        {/* <link rel="icon" type="image/x-icon" href="/favicon.ico" /> */}
+        <head>
+          {/* <!-- Meta Pixel Code --> */}
+          <Script id="fb-pixel" strategy="afterInteractive">
+            {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '842131638378353');
+      fbq('track', 'PageView');
+    `}
+          </Script>
 
-          <EdgeStoreProvider>
-            <NEXT_AUTH_PROVIDER>
+          <noscript>
+            <img height="1" width="1" style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=842131638378353&ev=PageView&noscript=1"
+            />
+          </noscript>
+          {/* <!-- End Meta Pixel Code --> */}
+        </head>
+
+
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+
+        <EdgeStoreProvider>
+          <NEXT_AUTH_PROVIDER>
             <LayoutWrapper>
-              
+
               {children}
               <Toaster position="top-center" reverseOrder={false} />
             </LayoutWrapper>
-            </NEXT_AUTH_PROVIDER>
-          </EdgeStoreProvider>
-       
+          </NEXT_AUTH_PROVIDER>
+        </EdgeStoreProvider>
+
 
       </body>
     </html>
