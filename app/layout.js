d88537a34833fa20b87c4ae93851732a9c8b41ca
Charham9087 <charhamqaiser45678@gmail.com>
2025-10-09 20:28:46 +0500

    --

--- app/layout.js
+++ app/layout.js
@@ -101,40 +101,38 @@ export default function RootLayout({ children }) {
           </NEXT_AUTH_PROVIDER>
         </EdgeStoreProvider>
 
-        {/* ✅ Facebook Meta Pixel Code */}
+       {/* ✅ Facebook Meta Pixel Code */}
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
-            !(function(f, b, e, v, n, t, s) {
-              if (f.fbq) return;
-              n = f.fbq = function() {
-                n.callMethod
-                  ? n.callMethod.apply(n, arguments)
-                  : n.queue.push(arguments);
+            !(function(f,b,e,v,n,t,s){
+              if(f.fbq)return;
+              n=f.fbq=function(){
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
               };
-              if (!f._fbq) f._fbq = n;
-              n.push = n;
-              n.loaded = !0;
-              n.version = "2.0";
-              n.queue = [];
-              t = b.createElement(e);
-              t.async = !0;
-              t.src = v;
-              s = b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t, s);
-            })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-
-            fbq("init", "1464772104830325");
-            fbq("track", "PageView");
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s);
+            })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+            
+            fbq('init', '1109494101397042');
+            fbq('track', 'PageView');
           `}
         </Script>
 
-        {/* ✅ NoScript fallback */}
+        {/* ✅ NoScript fallback (for users without JS) */}
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1464772104830325&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1109494101397042&ev=PageView&noscript=1"
             alt="facebook pixel"
           />
         </noscript>
