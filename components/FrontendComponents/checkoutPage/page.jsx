46c8bef5434545ed39a8c4d9c7b1dbbde964ea31
Charham9087 <charhamqaiser45678@gmail.com>
2025-10-24 21:41:57 +0500

    -

--- components/FrontendComponents/checkoutPage/page.jsx
+++ components/FrontendComponents/checkoutPage/page.jsx
@@ -255,7 +255,7 @@ export default function CheckoutPage() {
                     </div>
                   ))
                 ) : (
-                  <div className="text-center text-red-500">🔃 loading products  <br> Please Wait....</br></div>
+                  <div className="text-center text-red-500">🔃 loading products  <br/> Please Wait....</div>
                 )}
 
                 <div className="border-t pt-4 flex justify-between font-bold text-slate-800">
